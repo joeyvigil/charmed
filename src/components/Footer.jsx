ad6758fa79d18d25a4a823ee423a0ed51eb34a75
Joseph Vigil <joeyvigil109329@gmail.com>
2025-11-02 22:41:34 -0700

    footer added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,120 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="myfooter">
+    <div className="container "
+      style={{ paddingTop: '50px', borderTop: '1px solid var(--Base4)' }}>
+      <footer className="row mb-3">
+        <div className="col mb-3">
+          <a
+            href="/"
+            className="d-flex align-items-center mb-3 link-body-emphasis text-decoration-none"
+            aria-label="Bootstrap"
+          >
+            <svg className="bi me-2" width="40" height="32" aria-hidden="true">
+              <use xlinkHref="vite.svg"></use>
+            </svg>      
+            <svg className="bi me-2" width="40" height="32" aria-hidden="true">
+              <use xlinkHref="react.svg"></use>
+            </svg>
+            
+          </a>
+          <p className="text-body-secondary">charmed © 2025</p>
+        </div>
+        <div className="col mb-3"></div>
+        <div className="col mb-3">
+          <h5>Section</h5>
+          <ul className="nav flex-column">
+            <li className="nav-item mb-2">
+              <a href="#" className="nav-link p-0 text-body-secondary">
+                Home
+              </a>
+            </li>
+            <li className="nav-item mb-2">
+              <a href="#" className="nav-link p-0 text-body-secondary">
+                Features
+              </a>
+            </li>
+            <li className="nav-item mb-2">
+              <a href="#" className="nav-link p-0 text-body-secondary">
+                Pricing
+              </a>
+            </li>
+            <li className="nav-item mb-2">
+              <a href="#" className="nav-link p-0 text-body-secondary">
+                FAQs
+              </a>
+            </li>
+            <li className="nav-item mb-2">
+              <a href="#" className="nav-link p-0 text-body-secondary">
+                About
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="col mb-3">
+          <h5>Section</h5>
+          <ul className="nav flex-column">
+            <li className="nav-item mb-2">
+              <a href="#" className="nav-link p-0 text-body-secondary">
+                Home
+              </a>
+            </li>
+            <li className="nav-item mb-2">
+              <a href="#" className="nav-link p-0 text-body-secondary">
+                Features
+              </a>
+            </li>
+            <li className="nav-item mb-2">
+              <a href="#" className="nav-link p-0 text-body-secondary">
+                Pricing
+              </a>
+            </li>
+            <li className="nav-item mb-2">
+              <a href="#" className="nav-link p-0 text-body-secondary">
+                FAQs
+              </a>
+            </li>
+            <li className="nav-item mb-2">
+              <a href="#" className="nav-link p-0 text-body-secondary">
+                About
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="col mb-3">
+          <h5>Section</h5>
+          <ul className="nav flex-column">
+            <li className="nav-item mb-2">
+              <a href="#" className="nav-link p-0 text-body-secondary">
+                Home
+              </a>
+            </li>
+            <li className="nav-item mb-2">
+              <a href="#" className="nav-link p-0 text-body-secondary">
+                Features
+              </a>
+            </li>
+            <li className="nav-item mb-2">
+              <a href="#" className="nav-link p-0 text-body-secondary">
+                Pricing
+              </a>
+            </li>
+            <li className="nav-item mb-2">
+              <a href="#" className="nav-link p-0 text-body-secondary">
+                FAQs
+              </a>
+            </li>
+            <li className="nav-item mb-2">
+              <a href="#" className="nav-link p-0 text-body-secondary">
+                About
+              </a>
+            </li>
+          </ul>
+        </div>
+      </footer>
+    </div>
+    </div>
   )
 }
 
