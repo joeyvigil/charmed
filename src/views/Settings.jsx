ae6e9852536548eeea90a0a98cc90d1d2fbe4637
Joseph Vigil <joeyvigil109329@gmail.com>
2025-11-24 20:38:15 -0700

    Update Settings.jsx

--- src/views/Settings.jsx
+++ src/views/Settings.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Settings = ({ user }) => {
     const [background, setBackground] = useState(user?.video || 'cat_rain.mp4');
-    
+
   return (
     <>
       <video autoPlay muted loop id="myVideo2">
@@ -10,16 +10,7 @@ const Settings = ({ user }) => {
       </video>
 
       <div className='background'>
-        <div className="container">
-          <div className="row">
-            <div className="col-12 col-md-6">
-              <div className="small-container">
-                <h1 className="text-center">Settings</h1>
-                <p className="text-center">Manage your account settings from this panel.</p>
-              </div>
-            </div>
-          </div>
-        </div>
+
       </div>
     </>
   )
